011bc59f30ffe8d5e277cdd2cd9fc2cb5ca127c2
FinumSamBrun <samuel.brunner@finum.at>
2026-07-20 09:29:55 +0200

    fixed rebase bug

--- src/table/localization.js
+++ src/table/localization.js
@@ -40,44 +40,6 @@ class TableLocalizeController extends LocalizeController {
   }
 }
 
-class TableLocalizeController extends LocalizeController {
-  /**
-   * @returns {string}
-   */
-  lang() {
-    // @ts-ignore
-    const host =
-      /** @type {HTMLElement & { lang?: string; getRootNode?: () => ShadowRoot | Document }} */ (
-        this.host
-      );
-
-    const candidateElements = [];
-    let current = host;
-    const seen = new Set();
-    while (current && !seen.has(current)) {
-      seen.add(current);
-      candidateElements.push(current);
-      const rootNode = current.getRootNode?.();
-      if (rootNode instanceof ShadowRoot && rootNode.host) {
-        // @ts-ignore
-        current = /** @type {HTMLElement} */ (rootNode.host);
-      } else {
-        // @ts-ignore
-        current = /** @type {HTMLElement | null} */ (current.parentElement);
-      }
-    }
-
-    for (const candidate of candidateElements) {
-      const explicitLang = candidate.getAttribute?.('lang') || candidate.lang;
-      if (typeof explicitLang === 'string' && explicitLang.trim()) {
-        return explicitLang.toLowerCase();
-      }
-    }
-
-    return super.lang();
-  }
-}
-
 /**
  * @param {import('lit').ReactiveControllerHost & HTMLElement} host
  */
